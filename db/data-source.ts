ce9ea99656505754eb25dc4a77a5391ce7d18c7f
Abeera Raghib <abeeraraghib1212@gmail.com>
2025-10-23 17:10:54 +0500

    Updated pages

--- db/data-source.ts
+++ db/data-source.ts
@@ -1,12 +1,19 @@
-import 'dotenv/config';
-import { DataSourceOptions } from 'typeorm';
-
-export const dataSourceOptions: DataSourceOptions = {
-  type: 'postgres',
-  url: process.env.DATABASE_URL,
-  entities: ['dist/**/*.entity.js'],
-  synchronize: true,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-};
+import { DataSource, DataSourceOptions } from 'typeorm';
+ import * as dotenv from 'dotenv';
+ 
+ dotenv.config();
+ 
+ export const dataSourceOptions: DataSourceOptions = {
+   type: 'postgres',
+   host: process.env.DB_HOST,
+   port: Number(process.env.DB_PORT),
+   username: process.env.DB_USERNAME,
+   password: process.env.DB_PASSWORD,
+   database: process.env.DB_NAME,
+   entities: ['dist/**/*.entity{.js,.ts}'],
+   synchronize: true,
+ };
+ console.log(process.env.DB_HOST,process.env.DB_NAME, process.env.DB_PORT, process.env.DB_PASSWORD )
+ 
+ const dataSource = new DataSource(dataSourceOptions);
+ export default dataSource;
--- src/main.ts
+++ src/main.ts
@@ -1,44 +1,52 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import { json, urlencoded } from 'express';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { urlencoded, json } from 'express';
 import { MoviesModule } from './movies/movies.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
-  app.useGlobalPipes(new ValidationPipe());
-
-  // Enable CORS for your Netlify frontend
-  app.enableCors({
-    origin: [process.env.FRONTEND_URL],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
-  });
-
-  app.use(json({ limit: '50mb' }));
-  app.use(urlencoded({ limit: '50mb', extended: true }));
-
-  // Swagger setup (optional)
-  const builder = new DocumentBuilder()
-    .setTitle('Backend APIs')
-    .setDescription('This API suite serves the mobile clients.')
-    .setVersion('1.0')
-    .addBearerAuth()
-    .build();
-
-  const document = SwaggerModule.createDocument(app, builder, {
+ 
+ async function bootstrap() {
+   const app = await NestFactory.create(AppModule);
+ 
+   app.useGlobalPipes(new ValidationPipe());
+   app.enableCors({
+     origin: [
+       'http://localhost:5173'
+     ],
+     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+     allowedHeaders: ['Content-Type', 'Authorization'],
+     credentials: true,
+   });
+   app.use(json({ limit: '50mb' }));
+   app.use(urlencoded({ extended: true, limit: '50mb' }));
+ 
+   const builder = new DocumentBuilder()
+     .setTitle('Backend APIs')
+     .setDescription('This API suite serves the mobile clients.')
+     .setVersion('1.0')
+     .addBearerAuth(
+       {
+         type: 'http',
+         scheme: 'bearer',
+         bearerFormat: 'JWT',
+         name: 'JWT',
+         description:
+           'Enter JWT token to authenticate the service provider user',
+         in: 'header',
+       },
+      'JWT',
+     )
+     .build();
+   const document = SwaggerModule.createDocument(app, builder, {
     include: [MoviesModule],
-  });
-
-  SwaggerModule.setup('docs', app, document);
-
-  const PORT = parseInt(process.env.PORT || '5000', 10);
-  await app.listen(PORT, '0.0.0.0', () => {
-    Logger.log(`🚀 Server running on port ${PORT}`);
-  });
-}
-
-bootstrap();
+   }
+  )
+   SwaggerModule.setup('/', app, document);
+ 
+   const PORT = 5000;
+   await app.listen(PORT, () => {
+     Logger.log('[BaseUrl/api] Swagger Documentation Url');
+   });
+ }
+ 
+ bootstrap();
